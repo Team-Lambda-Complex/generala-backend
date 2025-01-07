e0cebf3d00fdf31ef24c3273820fea29beccda96
Juan Angel Basgall <juanangelbasgall@hotmail.com>
2025-01-06 22:04:45 -0300

    Initial commit

---
+++ src/main/java/com/lambda_complex/generala/GeneralaApplication.java
@@ -0,0 +1,13 @@
+package com.lambda_complex.generala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneralaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneralaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lambda_complex/generala/GeneralaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lambda_complex.generala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneralaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
